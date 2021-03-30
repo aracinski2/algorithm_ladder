2b802416d289d5192d941e1e2800a05a8daa86fe
Alex Racinski <aracinski2@gmail.com>
2021-03-29 19:11:32 -0500

    algorithm ladder basic array map double

--- basic_array.rb
+++ basic_array.rb
@@ -1,4 +1,4 @@
-# reduce sum
+# reduce: sum
 # numbers = [1, 2, 3, 4]
 # sum = 0
 # numbers.each do |number|
@@ -6,13 +6,22 @@
 # end
 # p sum
 
-numbers = [99, 101, 88, 4, 2000, 50]
-index = 0
-low_numbers = []
-while index < numbers.length
-  if numbers[index] < 100
-    low_numbers << numbers[index]
-  end
-  index += 1
+# select: less than 100
+# numbers = [99, 101, 88, 4, 2000, 50]
+# index = 0
+# low_numbers = []
+# while index < numbers.length
+#   if numbers[index] < 100
+#     low_numbers << numbers[index]
+#   end
+#   index += 1
+# end
+# p low_numbers
+
+# map: double
+numbers = [4, 2, 5, 99, -4]
+doubled = []
+numbers.each do |number|
+  doubled << number * 2
 end
-p low_numbers
+p doubled
