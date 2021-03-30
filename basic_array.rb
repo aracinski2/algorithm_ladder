67658e19a5abffa791b3c3b32ded318388fa456d
Alex Racinski <aracinski2@gmail.com>
2021-03-29 19:09:27 -0500

    algorithm ladder basic array select less than 100

--- basic_array.rb
+++ basic_array.rb
@@ -1,6 +1,18 @@
-numbers = [1, 2, 3, 4]
-sum = 0
-numbers.each do |number|
-  sum += number
+# reduce sum
+# numbers = [1, 2, 3, 4]
+# sum = 0
+# numbers.each do |number|
+#   sum += number
+# end
+# p sum
+
+numbers = [99, 101, 88, 4, 2000, 50]
+index = 0
+low_numbers = []
+while index < numbers.length
+  if numbers[index] < 100
+    low_numbers << numbers[index]
+  end
+  index += 1
 end
-p sum
+p low_numbers
