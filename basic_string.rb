6b00b816049f5c659ce8c398c48fd0f328ebed9b
Alex Racinski <aracinski2@gmail.com>
2021-03-30 19:45:23 -0500

    algorithm ladder basic string, show me the money

--- basic_string.rb
+++ basic_string.rb
@@ -1,11 +1,27 @@
 # reverse string
-def reverse(string)
-  reverse = ""
-  index = string.length - 1
-  while index >= 0
-    reverse << string[index]
-    index -= 1
+# def reverse(string)
+#   reverse = ""
+#   index = string.length - 1
+#   while index >= 0
+#     reverse << string[index]
+#     index -= 1
+#   end
+#   return reverse
+# end
+# p reverse("abcde")
+
+def money(string)
+  jackpot = false
+  index = 0
+  while index < string.length
+    if string[index] == "$"
+      jackpot = true
+      break
+    end
+    index += 1
   end
-  return reverse
+  return jackpot
 end
-p reverse("abcde")
+
+p money("i hate $ but i love money i know i know im crazy")
+p money("abcdefghijklmnopqrstuvwxyz")
