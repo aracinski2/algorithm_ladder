e8debacfa1a7903d298caa4215af5e848daaa219
Alex Racinski <aracinski2@gmail.com>
2021-03-30 20:09:36 -0500

    algorithm ladder basic string, alternate capitals

--- basic_string.rb
+++ basic_string.rb
@@ -10,18 +10,35 @@
 # end
 # p reverse("abcde")
 
-def money(string)
-  jackpot = false
+# def money(string)
+#   jackpot = false
+#   index = 0
+#   while index < string.length
+#     if string[index] == "$"
+#       jackpot = true
+#       break
+#     end
+#     index += 1
+#   end
+#   return jackpot
+# end
+
+# p money("i hate $ but i love money i know i know im crazy")
+# p money("abcdefghijklmnopqrstuvwxyz")
+
+def alternate(string)
   index = 0
+  capitalized = ""
   while index < string.length
-    if string[index] == "$"
-      jackpot = true
-      break
+    if string[index] == " "
+      index += 1
     end
+    capitalized << string[index]
+    index += 1
+    capitalized << string[index].upcase
     index += 1
   end
-  return jackpot
+  return capitalized
 end
 
-p money("i hate $ but i love money i know i know im crazy")
-p money("abcdefghijklmnopqrstuvwxyz")
+p alternate("hello, how are your porcupines today?")
