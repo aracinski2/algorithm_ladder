bf602c2040c624da83f02e6da9e5c6f2e2bb8717
Alex Racinski <aracinski2@gmail.com>
2021-04-02 18:57:28 -0500

    algorithm ladder, two pointers, largest product

--- two_pointers.rb
+++ two_pointers.rb
@@ -16,17 +16,36 @@
 # p combo(["a", "b", "c"], ["d", "e", "f", "g"])
 
 # array mesh II
-letters = ["a", "b", "c", "d"]
-combo_array = []
-index = 0
-while index < letters.length
-  index2 = 0
-  while index2 < letters.length
-    if index2 != index
-      combo_array << letters[index] + letters[index2]
+# letters = ["a", "b", "c", "d"]
+# combo_array = []
+# index = 0
+# while index < letters.length
+#   index2 = 0
+#   while index2 < letters.length
+#     if index2 != index
+#       combo_array << letters[index] + letters[index2]
+#     end
+#     index2 += 1
+#   end
+#   index += 1
+# end
+# p combo_array
+
+# largest product
+def largest_product(array)
+  index = 0
+  largest = array[index]
+  while index < array.length
+    index2 = index + 1
+    while index2 < array.length
+      product = array[index] * array[index2]
+      if product > largest
+        largest = product
+      end
+      index2 += 1
     end
-    index2 += 1
+    index += 1
   end
-  index += 1
+  return largest
 end
-p combo_array
+p largest_product([5, -2, 1, -9, -7, 2, 6])
