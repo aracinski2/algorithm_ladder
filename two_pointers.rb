6dbd8c4d68ecb04f59bf08f7aafa786c89f01918
Alex Racinski <aracinski2@gmail.com>
2021-04-02 18:52:23 -0500

    algorithm ladder, two pointers, array mesh II

--- two_pointers.rb
+++ two_pointers.rb
@@ -1,16 +1,32 @@
 # array mesh I
-def combo(array1, array2)
-  index = 0
-  combo_array = []
-  while index < array1.length
-    index2 = 0
-    while index2 < array2.length
-      combo_array << array1[index] + array2[index2]
-      index2 += 1
-    end
-    index += 1
-  end
-  return combo_array
-end
+# def combo(array1, array2)
+#   index = 0
+#   combo_array = []
+#   while index < array1.length
+#     index2 = 0
+#     while index2 < array2.length
+#       combo_array << array1[index] + array2[index2]
+#       index2 += 1
+#     end
+#     index += 1
+#   end
+#   return combo_array
+# end
 
-p combo(["a", "b", "c"], ["d", "e", "f", "g"])
+# p combo(["a", "b", "c"], ["d", "e", "f", "g"])
+
+# array mesh II
+letters = ["a", "b", "c", "d"]
+combo_array = []
+index = 0
+while index < letters.length
+  index2 = 0
+  while index2 < letters.length
+    if index2 != index
+      combo_array << letters[index] + letters[index2]
+    end
+    index2 += 1
+  end
+  index += 1
+end
+p combo_array
