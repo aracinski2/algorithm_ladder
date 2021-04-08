6e17c36c3464faa05f3568ff658fe866f572e205
Alex Racinski <aracinski2@gmail.com>
2021-04-08 15:36:42 -0500

    algorithm ladder, two pointers, merge sorted arrays

--- two_pointers.rb
+++ two_pointers.rb
@@ -51,26 +51,48 @@
 # p largest_product([5, -2, 1, -9, -7, 2, 6])
 
 # two sum I
-def sum_of_ten(array)
-  index = 0
-  sum = 0
-  while index < array.length
-    index2 = index + 1
-    while index2 < array.length
-      sum = array[index] + array[index2]
-      if sum == 10
-        return [array[index], array[index2]]
-        break
-      end
+# def sum_of_ten(array)
+#   index = 0
+#   sum = 0
+#   while index < array.length
+#     index2 = index + 1
+#     while index2 < array.length
+#       sum = array[index] + array[index2]
+#       if sum == 10
+#         return [array[index], array[index2]]
+#         break
+#       end
 
+#       index2 += 1
+#     end
+#     index += 1
+#   end
+#   if index == array.length
+#     return false
+#   end
+# end
+
+# p sum_of_ten([2, 5, 3, 1, 0, 7, 11])
+# p sum_of_ten([1, 2, 3, 4, 5])
+
+# merge sorted arrays
+array = [1, 5, 8]
+array2 = [6, 9]
+index = 0
+index2 = 0
+while index < array.length
+  while index2 < array2.length
+    if array2[index2] < array[index]
+      array.insert(index, array2[index2])
       index2 += 1
+    elsif index == array.length - 1
+      array.insert(index + 1, array2[index2])
+      index2 += 1
+    else
+      index += 1
     end
-    index += 1
+    # p array.length
   end
-  if index == array.length
-    return false
-  end
+  break
 end
-
-p sum_of_ten([2, 5, 3, 1, 0, 7, 11])
-p sum_of_ten([1, 2, 3, 4, 5])
+p array
