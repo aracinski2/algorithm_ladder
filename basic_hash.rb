35ae96c31b01915b9f5c00f83ebdc8d7ae513a1d
Alex Racinski <aracinski2@gmail.com>
2021-04-11 12:32:43 -0500

    algorithm ladder, basic hash, rna transcription

--- basic_hash.rb
+++ basic_hash.rb
@@ -1,30 +1,30 @@
 # most frequent letter
-def most_frequent_letter(string)
-  index = 0
-  letters = Hash.new
-  most_frequent = 0
-  while index < string.length
-    letter = string[index]
-    # p letters[letter]
-    if letters.has_key?(letter)
-      letters[letter] += 1 
-    else
-      letters[letter] = 1
-    end
-    index += 1
-  end
-  letters.each do |k,v|
-    if v > most_frequent
-      most_frequent = v
-    end
-  end
-  return most_frequent
-end
+# def most_frequent_letter(string)
+#   index = 0
+#   letters = Hash.new
+#   most_frequent = 0
+#   while index < string.length
+#     letter = string[index]
+#     # p letters[letter]
+#     if letters.has_key?(letter)
+#       letters[letter] += 1 
+#     else
+#       letters[letter] = 1
+#     end
+#     index += 1
+#   end
+#   letters.each do |k,v|
+#     if v > most_frequent
+#       most_frequent = v
+#     end
+#   end
+#   return most_frequent
+# end
 
-p most_frequent_letter("peter piper picked a peck of pickled peppers")
+# p most_frequent_letter("peter piper picked a peck of pickled peppers")
 
-# count votes
-# def vote_count(array)
+# # count votes
+# # def vote_count(array)
 #   votes = Hash.new
 #   array.each do |vote|
 #     # p votes[vote]
@@ -46,3 +46,21 @@ p most_frequent_letter("peter piper picked a peck of pickled peppers")
 #   order_total += v
 # end
 # p order_total
+
+# RNA transcription
+def rna_transcription(string)
+  rna_complements = {"G": "C", "C": "G", "T": "A", "A": "U"}
+  transcribed_rna = ""
+  index = 0
+  # p nucleotide = string[index]
+  # p rna_complements[index]
+  while index < string.length
+    nucleotide = string[index]
+    rna_complements[nucleotide.to_sym]
+    transcribed_rna += rna_complements[nucleotide.to_sym]
+    index += 1
+  end
+  return transcribed_rna
+end
+
+p rna_transcription('ACGTGGTCTTAA')
